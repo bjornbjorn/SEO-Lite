da3573e293252f697e071d76ed12fcce5e0e45d5
Bjørn Børresen <mail@bybjorn.com>
2011-03-03 09:05:07 +0100

    french language added thanks to Nico Prat

---
+++ seo_lite/language/french/seo_lite_lang.php
@@ -0,0 +1,22 @@
+<?php
+
+$lang['seo_lite'] = 'SEO Lite';
+$lang['seo_lite_module_name'] = "SEO Lite";
+$lang['seo_lite_module_description'] = "One calorie SEO module, no sugar added!";
+$lang['welcome'] = "Bienvenue sur SEO Lite";
+$lang['default_keywords'] = "Mots clés par défaut de la page";
+$lang['default_description'] = "Description par défaut de la page";
+$lang['current_value'] = 'Valeur Actuelle';
+$lang['settings_saved'] = 'Options de SEO Lite sauvegardées';
+$lang['seotitle'] = 'SEO Titre';
+$lang['title_instructions'] = "Optionnel, si vous n'indiquez rien, le titre de la page sera utilisé. N'oubliez pas que la plupart des moteurs de recherche n'utilisent que 60 caractères maximum.";
+$lang['seokeywords'] = 'SEO Mots clés';
+$lang['keywords_instructions'] = 'Une liste de mots clés séparés par des virgules.';
+$lang['seodescription'] = 'SEO Description';
+$lang['description_instructions'] = 'Renseignez une ou deux courtes phrases qui résument le contenu de la page (la plupart des moteurs de recherche afficheront ceci dans les résultats de recherche)';
+$lang['default_title_postfix'] = 'Séparateur de titre par défaut';
+$lang['default_title_postfix_description'] = "Ce que vous indiquez ici sera ajouté après le tag {title}, à moins que cette configuration ne soit écrasée par le paramètre <em>title_postfix</em> dans le tag.";
+
+
+$lang['docs'] = 'Documentation';
+$lang['settings'] = 'Options';
