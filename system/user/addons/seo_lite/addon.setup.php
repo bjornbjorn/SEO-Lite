db61fa11c3c65d2dc870fe9335c809a652c86aa8
Bjørn Børresen <bjorn@lastfriday.com>
2018-12-17 22:12:07 +0100

    Version update

--- system/user/addons/seo_lite/addon.setup.php
+++ system/user/addons/seo_lite/addon.setup.php
@@ -5,7 +5,7 @@ return array(
     'author_url'  => 'http://wedoaddons.com',
     'name'        => 'SEO Lite',
     'description' => 'The no-brainer approach to SEO for ExpressionEngine.',
-    'version'     => '1.5.5',
+    'version'     => '1.5.6',
     'namespace'   => 'Wedoaddons\SEO-Lite',
     'settings_exist' => TRUE,
     'docs_url' => 'http://wedoaddons.com/addon/seo-lite',
